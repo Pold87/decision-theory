b11e7c66e3407eaec997666b9c6539fb747f8e12
Volker Strobel <volker.strobel87@gmail.com>
2016-06-10 12:05:25 +0200

    minor

--- R/copula_tests.R
+++ R/copula_tests.R
@@ -1,11 +1,6 @@
 require(copula)
 require(VineCopula)
 
-## Convert a sample data matrix to data with uniform margins on [0, 1]
-to.copuladata <- function(df) {
-    rank(df)
-}
-
 ## Read R, G, B data and corresponding x, y-positions
 X <- read.csv("../sample_data.csv", header = T)
 
@@ -15,19 +10,14 @@ X.cop.df <- pobs(X, ties.method = 'random')
 ## Convert to a copuladata object  (as used by the Vine Copula package)
 X.cop <- as.copuladata(X.cop.df)
 
-
 pairs.copuladata(X.cop)
 
+rvm <- RVineStructureSelect(X.cop)
 
-# define 5-dimensional R-vine tree structure matrix
-Matrix <- c(5, 2, 3, 1, 4,
-            0, 2, 3, 4, 1,
-            0, 0, 3, 4, 1,
-            0, 0, 0, 4, 1,
-            0, 0, 0, 0, 1)
-Matrix <- matrix(Matrix, 5, 5)
+cvm <- RVineStructureSelect(X.cop, type="CVine")
 
-RVC <- RVineStructureSelect(X.cop)
-
-RVineTreePlot(data=NULL, RVM=RVC, tree=2,
+RVineTreePlot(data=NULL, RVM=cvm, tree=1,
               edge.labels=c("family","theotau"))
+
+
+RVineVuongTest(X.cop, rvm, cvm)
