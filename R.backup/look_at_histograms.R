43d49f1e25ed283515271e49ebc1f260907c9495
Volker Strobel <volker.strobel87@gmail.com>
2016-06-24 10:34:22 +0200

    .gitignore is now working

--- R.backup/look_at_histograms.R
+++
@@ -1,9 +0,0 @@
-require(psych)
-require(copula)
-require(VineCopula)
-
-X <- read.csv('/home/pold/Documents/tudelft/advanced_decision/algo/sample_data.csv')
-
-X.cop <- as.copuladata(X)
-
-pairs.panels(X)
